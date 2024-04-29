090ebcc8b84b34a3de0fd2c4122c3868b3021f51
David Cardinal <github@cardinalphoto.com>
2024-04-29 14:41:04 -0700

    fix complex number issue

--- utilities/hdrVideoClip.m
+++ utilities/hdrVideoClip.m
@@ -5,28 +5,33 @@ videoFPS = 2; % show slowly
 
 hdrList = dir(filespec);
 [hdrPath, basename, ~] = fileparts(filespec);
+basename = replace(basename,'*','');
 outputVideoPrefix = fullfile(hdrPath, [basename '-video']);
 
 
 if isunix
-    demoVideo = VideoWriter(outputVideoPrefix,  'Uncompressed AVI'); % 'Motion JPEG AVI');
+    %demoVideo = VideoWriter(outputVideoPrefix,  'Uncompressed AVI'); % 'Motion JPEG AVI');
+    demoVideo = vision.VideoFileWriter([outputVideoPrefix '.avi']); % 'Motion JPEG AVI');
 else
     % H.264 only works on Windows and Mac
-    demoVideo = VideoWriter(outputVideoPrefix, 'MPEG-4');
+    %demoVideo = VideoWriter(outputVideoPrefix, 'MPEG-4');
+    demoVideo = vision.VideoFileWriter([outputVideoPrefix '.mp4'], 'FileFormat','MPEG4');
 end
 
 demoVideo.FrameRate = videoFPS;
 % Don't set quality if archival
 %demoVideo.Quality = 90;
-open(demoVideo);
+%open(demoVideo);
 for ii = 1:numel(hdrList)
     hdr = exrread(fullfile(hdrList(ii).folder,hdrList(ii).name));
+    hdr = max(hdr,0);
     % DOESN"T WORK rgb = tonemap(real(hdr));
     rgb = single(hdrRender(hdr));
     %imshow(rgb);
-    writeVideo(demoVideo,rgb);
+    %writeVideo(demoVideo,rgb);
+    demoVideo(rgb);
 end
-close (demoVideo);
-
+%close (demoVideo);
+release(demoVideo);
 end
 
