f550402e9a105275e723b74775fc97c48eb854f8
David Cardinal <github@cardinalphoto.com>
2024-04-29 10:54:49 -0700

    function to generate preview videos from exr files

---
+++ utilities/hdrVideoClip.m
@@ -0,0 +1,27 @@
+function outputVideo = hdrVideoClip(filespec)
+%HDRVIDEOCLIP Create a video from a list of .exr files
+
+hdrList = dir(filespec);
+[hdrPath, basename, ~] = fileparts(filespec);
+outputVideoPrefix = fullfile(hdrPath, [basename '-video']);
+
+
+if isunix
+    demoVideo = VideoWriter(outputVideoPrefix, 'Motion JPEG AVI');
+else
+    % H.264 only works on Windows and Mac
+    demoVideo = VideoWriter(outputVideoPrefix, 'MPEG-4');
+end
+
+demoVideo.FrameRate = videoFPS;
+demoVideo.Quality = 99;
+open(demoVideo);
+for ii = 1:numel(hdrList)
+    hdr = hdrread(hdrList(ii));
+    rgb = tonemap(hdr);
+    writeVideo(demoVideo,rgb);
+end
+close (demoVideo);
+
+end
+
