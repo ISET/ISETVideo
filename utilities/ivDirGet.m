38fbeb54ec66b4ec782fe9092509a25d2f6e061e
David Cardinal <github@cardinalphoto.com>
2024-05-11 11:39:51 -0700

    add wsl support

--- utilities/ivDirGet.m
+++ utilities/ivDirGet.m
@@ -4,10 +4,20 @@ function ourDir = ivDirGet(dirType)
 % Not exciting now, but once we start using datasets of frames
 % and clips from acorn as well as locally, it'll get more interesting
 
+% for host specific uses:
+% get our hostName
+[~, hostName] = system('hostname');
+hostName = lower(strtrim(hostName));
+
 rootDir = ivRootPath();
+wslRootDir = '\\wsl$\Ubuntu-20.04\home\djc\iset\isetvideo\';
 switch dirType
     case 'local'
+    if isequal(hostName, 'decimate') && ispc
+        ourDir = fullfile(wslRootDir, 'local');
+    else
         ourDir = fullfile(rootDir, 'local');
+    end
     case 'data'
         ourDir = fullfile(rootDir, 'data');
 end
